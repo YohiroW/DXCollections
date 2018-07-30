49ba808a4853fc585958d8a8581b30fc51fa3588
YohiroW <tz281678310@sina.com>
2018-07-30 22:57:48 +0800

    Fixed compile errors

--- Common/Globals.h
+++ Common/Globals.h
@@ -1,9 +1,6 @@
 #ifndef _GLOBALS_H_
 #define _GLOBALS_H_
-<<<<<<< HEAD
 #include <memory>
-=======
->>>>>>> dev_fbxsdk
 #include "windows.h"
 #include "xnamath.h"
 #include "d3d11.h"
@@ -22,11 +19,7 @@
             _ptr_ = nullptr;     \
 		}
 
-<<<<<<< HEAD
-
 const float PI = 3.1415926f;
-=======
->>>>>>> dev_fbxsdk
 #define TRIANGLE_VERTEX_COUNT   3
 
 typedef XMFLOAT4 Vec4;
@@ -35,14 +28,9 @@ typedef XMFLOAT2 Vec2;
 
 struct GeneralVertex
 {
-<<<<<<< HEAD
 	Vec3 Position;
 	Vec3 Normal;       // As color for now
 	Vec2 UV;
-=======
-	Vec4 Position;
-	Vec4 Normal;       // As color for now
->>>>>>> dev_fbxsdk
 };
 
 struct BasicTransform
